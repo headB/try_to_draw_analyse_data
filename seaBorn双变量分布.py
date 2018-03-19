f064b3ce397f6bd75f9ba0fb374575879f555974
beetle <775121173@qq.com>
2018-03-19 14:26:55 +0800

    Update and rename 双变量分布.py to seaBorn双变量分布.py

--- seaBorn双变量分布.py
+++ seaBorn双变量分布.py
@@ -13,6 +13,9 @@ def_obj2 = pd.DataFrame({"x":np.random.randn(500),
                         })
                         
                         
-##例子1
+##例子1--散布图
 sns.jointplot(x="x",y="y",data=def_obj1)
+
+##例子2--二维直方图
+sns.jointplot(x="x",y="y",data=def_obj2,kind="hex")
 
