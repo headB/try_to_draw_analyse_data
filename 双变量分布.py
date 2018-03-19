8e5b08062865ab4f9b99482154199b382dc15a81
beetle <775121173@qq.com>
2018-03-19 14:19:14 +0800

    Create 双变量分布.py

---
+++ 双变量分布.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats, integrate
+import seaborn as sns
+%matplotlib inline
+
+def_obj1 = pd.DataFrame({"x":np.random.randn(500),
+                        "y":np.random.randn(500)})
+
+def_obj2 = pd.DataFrame({"x":np.random.randn(500),
+                        "y":np.random.randint(0,100,500)
+                        })
+                        
+                        
+##例子1
+sns.jointplot(x="x",y="y",data=def_obj1)
+
