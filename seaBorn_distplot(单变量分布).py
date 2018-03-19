b36c4c6d049fe2604814a69ca5a357b80532b8b2
beetle <775121173@qq.com>
2018-03-19 11:37:33 +0800

    Update seaBorn_distplot(单变量分布).py

--- seaBorn_distplot(单变量分布).py
+++ seaBorn_distplot(单变量分布).py
@@ -1,11 +1,12 @@
-##注意,是在ipython3-notebook下面执行的
+##注意,是在ipython3-notebook下面执行的,
+##或者都可以在ipython运行,不过启动的时候,命令变了,ipython qtconsole这样启动.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib inline
 
-##注意,x1或者x2,随便一个,不能同时运行两个.不行图形就会出现问题.!
+##注意,x1或者x2,x3等等,几个画图都是需要注释,只能运行其中一个,随便一个,不能同时运行两个.不行图形就会出现问题.!
 
 x1 = np.random.normal(size=1000)
 sns.distplot(x1)
