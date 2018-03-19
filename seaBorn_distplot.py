b76d8cd450d17d05ecb091a425091a31a19f2835
beetle <775121173@qq.com>
2018-03-19 10:28:17 +0800

    Update seaBorn_distplot.py

--- seaBorn_distplot.py
+++ seaBorn_distplot.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib inline
 
+##注意,x1或者x2,随便一个,不能同时运行两个.不行图形就会出现问题.!
+
 x1 = np.random.normal(size=1000)
 sns.distplot(x1)
 
-x2 = np.random.randint(0,100,500)
-sns.distplot(x2)
+#x2 = np.random.randint(0,100,500)
+#sns.distplot(x2)
